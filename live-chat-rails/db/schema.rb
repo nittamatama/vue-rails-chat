2e33651c14ec5226c9dc774da14c4c11686c4df7
nittamatama <blackmnshin@gmail.com>
2022-07-24 16:12:42 +0900

    messagesテーブル

--- live-chat-rails/db/schema.rb
+++ live-chat-rails/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_24_032326) do
+ActiveRecord::Schema.define(version: 2022_07_24_070736) do
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
