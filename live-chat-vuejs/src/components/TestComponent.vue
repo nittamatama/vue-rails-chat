6685e8fd891cfafefb529e75f13ceab1ce17ffba
nittamatama <blackmnshin@gmail.com>
2022-07-29 21:47:32 +0900

    components配下にテストコンポーネント作成してビューで読み込み

---
+++ live-chat-vuejs/src/components/TestComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    これはコンポーネントから表示されています。
+  </div>
+</template>
--- live-chat-vuejs/src/views/Welcome.vue
+++ live-chat-vuejs/src/views/Welcome.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @click="toggle">トグルする</button>
-    <p>{{ text }}</p>
+    <TestComponent />
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
+
 export default {
+  components: {
+    TestComponent
+  },
   data() {
     return {
       title: '初めてのvue',
