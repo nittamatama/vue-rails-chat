4b77fa6fa2597c584ee0cac9ca86be06e728d095
nittamatama <blackmnshin@gmail.com>
2022-07-29 21:15:52 +0900

    vuetest

---
+++ live-chat-vuejs/src/views/Welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p v-if="isEnabled">{{ subtitle }}</p>
+    <button @click="toggle">トグルする</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '初めてのvue',
+      subtitle: 'ようこそ',
+      isEnabled: true
+    }
+  },
+  computed: {
+    text() {
+      if (this.isEnabled) {
+        return 'こんにちは'
+      } else {
+        return 'さようなら'
+      }
+    }
+  },
+  methods: {
+    toggle() {
+      this.isEnabled = !this.isEnabled
+    }
+  }
+}
+</script>
