2f7c8e53313c39d72e751643606c87fb303575e3
nittamatama <blackmnshin@gmail.com>
2022-07-30 17:20:13 +0900

    spanタグに@clickディレクティブを当てて同一ページ内で表示が切り替わる

--- live-chat-vuejs/src/views/Welcome.vue
+++ live-chat-vuejs/src/views/Welcome.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container welcome">
     <p>ようこそ！</p>
-    <LoginForm />
-    <SignupForm />
+    <div v-if="shouldShowLoginForm">
+      <LoginForm />
+      <p class="change-form">初めての方は<span @click="shouldShowLoginForm = false">こちら</span>をクリック</p>
+    </div>
+    <div v-if="!shouldShowLoginForm">
+      <SignupForm />
+      <p class="change-form">アカウントをお持ちの方は<span @click="shouldShowLoginForm = true">こちら</span>をクリック</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +18,11 @@ import LoginForm from '../components/LoginForm.vue'
 import SignupForm from '../components/SignupForm.vue'
 export default {
   components: { LoginForm, SignupForm },
+  data() {
+    return {
+      shouldShowLoginForm: false
+    }
+  }
 }
 </script>
 
@@ -45,5 +56,10 @@ export default {
   }
   .welcome button {
     margin: 20px auto;
+  }
+
+  .change-form {
+    font-size: 14px;
+    margin: 10px;
   }
 </style>
